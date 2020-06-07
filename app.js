01ed7335070f079d8dbadd9a4848a382dc9ae596
Tony <waiting33118@gmail.com>
2020-06-07 16:58:12 +0800

    feat: use passport & set env

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const express = require('express')
+const session = require('express-session')
 const routes = require('./routes')
-const exphbs = require('express-handlebars')
+const exphdbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const Restaurant = require('./models/restaurant')
+const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 const app = express()
-const hostname = `127.0.0.1`
-const port = 3000
+const PORT = process.env.PORT
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(express.static('public'))
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.alert_msg = req.flash('alert_msg')
+  res.locals.success_msg = req.flash('success_msg')
+  next()
+})
 app.use(routes)
 
-app.listen(port, hostname, () => {
-	console.log(`The Server is running on http://${hostname}:${port}`)
+app.listen(PORT, () => {
+  console.log(`The Server is running on Port:${PORT}`)
 })
