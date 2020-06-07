6cec1d54a914274b5b4e6ef04f18518eb5ebe89b
Tony <waiting33118@gmail.com>
2020-06-07 20:49:48 +0800

    feat: add facebook router & use auth middleware

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
-router.use('/', home)
+router.use('/auth', auth)
+router.use('/', authenticator, home)
 
 module.exports = router
