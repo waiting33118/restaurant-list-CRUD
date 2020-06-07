14dd61a2151a5c61acb6ec260c5bb9e0d177f5ac
Tony <waiting33118@gmail.com>
2020-06-07 20:49:12 +0800

    feat: use facebook strategy & breate password by bcrypt

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 module.exports = (app) => {
@@ -19,12 +21,45 @@ module.exports = (app) => {
                 req.flash('alert_msg', '此帳號尚未註冊！')
               )
             }
-            if (password !== user.password) {
+            bcrypt.compare(password, user.password).then((isMatch) => {
+              if (isMatch) {
+                return done(null, user)
+              }
               return done(null, false, req.flash('alert_msg', '密碼錯誤！'))
-            }
-            return done(null, user)
+            })
           })
           .catch((err) => done(err))
+      }
+    )
+  )
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName']
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json
+        User.findOne({ email }).then((user) => {
+          if (user) return done(null, user)
+          // 若無帳號紀錄則建立
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(randomPassword, salt))
+            .then((hash) => {
+              return User.create({
+                name,
+                email,
+                password: hash
+              })
+            })
+            .then((user) => done(null, user))
+            .catch((err) => console.log(err))
+        })
       }
     )
   )
